87d599b553f2a11073e54bca70297980cca70a6c
Dominik Gresch <greschd@gmx.ch>
2021-10-07 23:37:50 +0200

    Add example for run with BatchSubmitter

--- examples/search/periodic_line_batch_submit/run.py
+++ examples/search/periodic_line_batch_submit/run.py
@@ -6,27 +6,34 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 import nodefinder as nf
 
+from fsc.async_tools import BatchSubmitter
 
-def gap_fct(pos):
+
+def gap_fct(pos_list):
     radius = 0.2
-    delta = (np.array(pos) - [0, 0.5, 0.5]) % 1
+    # print(len(pos_list))
+    delta = (np.array(pos_list) - np.array([0, 0.5, 0.5])) % 1
     delta = np.minimum(delta, 1 - delta)
-    dx, dy, dz = delta
-    return np.sqrt(np.abs(dx**2 + dy**2 - radius**2) + dz**2)
+    return np.sqrt(
+        np.abs(delta[:, 0]**2 + delta[:, 1]**2 - radius**2) + delta[:, 2]**2
+    )
 
+
+gap_fct_batched = BatchSubmitter(gap_fct, timeout=0, max_batch_size=100)
 
 if __name__ == "__main__":
 
     result = nf.search.run(
-        gap_fct,
+        gap_fct_batched,
         initial_mesh_size=(3, 3, 3),
         gap_threshold=2e-4,
         feature_size=2e-2,
         use_fake_potential=True,
+        num_minimize_parallel=100,
     )
     nf.io.save(result, "result.hdf5")
     nf.search.plot.points(result)
     plt.show()
+    plt.savefig("line.pdf", bbox_inches="tight")
